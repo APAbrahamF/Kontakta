f6abad3c6149e84973cd89f7ea6c1cf22e906c67
axelmz <56595610+axelmz@users.noreply.github.com>
2020-10-28 16:36:36 -0600

    Vista de activity para Cambio_usuario y cambio_prestador
    Pues lo del titulo bro

--- Kontakta/app/src/main/java/com/example/kontakta/CambioDatosPrestador.kt
+++ Kontakta/app/src/main/java/com/example/kontakta/CambioDatosPrestador.kt
@@ -1,4 +1,20 @@
 package com.example.kontakta
 
-class CambioDatosPrestador {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class CambioDatosPrestador : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cambio_prestador)
+        var BotonGuardar: FloatingActionButton = findViewById(R.id.FABGuardarP) as FloatingActionButton;
+        BotonGuardar.setOnClickListener {
+            guardar();
+        }
+    }
+    private fun guardar(){
+        //Metodo prron donde se van a guardar los cambios
+    }
 }
--- Kontakta/app/src/main/java/com/example/kontakta/CambioDatosUsuario.kt
+++ Kontakta/app/src/main/java/com/example/kontakta/CambioDatosUsuario.kt
@@ -1,4 +1,21 @@
 package com.example.kontakta
 
-class CambioDatosUsuario {
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class CambioDatosUsuario : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cambio_usuario)
+        var BotonGuardar: FloatingActionButton = findViewById(R.id.FABGuardarU) as FloatingActionButton;
+        BotonGuardar.setOnClickListener {
+            guardar();
+        }
+    }
+    private fun guardar(){
+        //Metodo prron donde se van a guardar los cambios
+    }
 }
