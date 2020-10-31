483188dde3b8a9ed324422099522aaa152e94f3c
APAbrahamF <47760074+APAbrahamF@users.noreply.github.com>
2020-10-31 17:55:30 -0600

    Se agrego el boton de configuracion

--- Kontakta/app/src/main/java/com/example/kontakta/MenuConfiguracion.kt
+++ Kontakta/app/src/main/java/com/example/kontakta/MenuConfiguracion.kt
@@ -1,4 +1,11 @@
 package com.example.kontakta
 
-class MenuConfiguracion {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuConfiguracion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.configuracion)
+    }
 }
--- Kontakta/app/src/main/java/com/example/kontakta/MenuPrincipal.kt
+++ Kontakta/app/src/main/java/com/example/kontakta/MenuPrincipal.kt
@@ -1,11 +1,18 @@
 package com.example.kontakta
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
 
 class MenuPrincipal: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu_principal)
+        var buttConf:ImageButton = findViewById(R.id.confButton) as ImageButton
+        buttConf.setOnClickListener{
+            val intent = Intent(this,MenuConfiguracion::class.java)
+            startActivity(intent)
+        }
     }
 }
