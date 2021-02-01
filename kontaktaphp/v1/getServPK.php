9fd86f2ccb8d8f1a130f8e7a6a1b639ee4e1550a
hradium <hazerful@live.com>
2021-01-31 20:14:41 -0600

    se agrego getServPK.php

---
+++ kontaktaphp/v1/getServPK.php
@@ -0,0 +1,35 @@
+<?php 
+	
+	//Aqui lo que hago es ver si ya me pasaron el correo y luego hago la sentencia, paso los datos a un array y lo regreso 
+	require_once '../DbConnect.php';
+   $db = new DbConnect();   
+   $con = $db->connect();
+   
+   $array = array();	
+   if(isset($_POST['IDServ'])){
+      	$IDServ = $_POST['IDServ'];   		
+     	$sql = "SELECT * FROM prestadorservicios WHERE 	IDServicio= '$IDServ'";
+   if($resultset = mysqli_query($con, $sql)){
+	while ($row = mysqli_fetch_assoc($resultset)){
+		$e = array();
+		$e['nombreServicio'] = $row['nombreServicio'];
+		$e['imagen'] = $row['imagen'];
+		$e['integrantes'] = $row['integrantes'];
+		$e['descripcion'] = $row['descripcion'];
+		$e['genero'] = $row['genero'];
+		$e['youtube'] = $row['youtube'];
+		$e['instagram'] = $row['instagram'];
+		$e['facebook'] = $row['facebook'];
+		$e['twitter'] = $row['twitter']; 		
+      	$e['message'] = 'Sirve y muestra';
+      array_push($array,$e);
+	}
+   }
+   }
+   else{
+      $array['message'] = "No se puede conectar";
+   }
+   echo json_encode($array);
+
+?>
+
