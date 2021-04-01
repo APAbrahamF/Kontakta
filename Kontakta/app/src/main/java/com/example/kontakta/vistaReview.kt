f746fd8826b396e048e345fd666217f15edffb7b
hradium <hazerful@live.com>
2021-04-01 12:15:10 -0600

    adicion de interfaz para vistaReview.kt

---
+++ Kontakta/app/src/main/java/com/example/kontakta/vistaReview.kt
@@ -0,0 +1,11 @@
+package com.example.kontakta
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class vistaReview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.vista_review)
+    }
+}
