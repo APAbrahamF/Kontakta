96c918791b4233f92a742aaf6d61219f63317438
hradium <hazerful@live.com>
2021-06-08 22:35:57 -0500

    agrego nuevos parametros a insertHistorial.php

--- kontaktaphp/v1/insertHistorial.php
+++ kontaktaphp/v1/insertHistorial.php
@@ -5,9 +5,9 @@
   
   //response array 
     $response = array(); 
-    if (isset($_POST['nombrePrestador']) && isset($_POST['imagenPrestador']) && isset($_POST['IDServicio_FK']) && isset($_POST['IDUsuario_FK'])) {
+    if (isset($_POST['nombrePrestador']) && isset($_POST['imagenPrestador']) && isset($_POST['estadoUser']) && isset($_POST['municipioUser']) && isset($_POST['sexoUser']) && isset($_POST['edadUser']) && isset($_POST['IDServicio_FK']) && isset($_POST['IDUsuario_FK'])) {
         $db = new DbOperation(); 
-        if($db->insertHistorial($_POST['nombrePrestador'], $_POST['imagenPrestador'], $_POST['IDServicio_FK'], $_POST['IDUsuario_FK'])){
+        if($db->insertHistorial($_POST['nombrePrestador'], $_POST['imagenPrestador'], $_POST['estadoUser'], $_POST['municipioUser'], $_POST['sexoUser'], $_POST['edadUser'], $_POST['IDServicio_FK'], $_POST['IDUsuario_FK'])){
            $response['error'] = false;
            $response['message'] = 'Agregado correctamente';
         }else{
