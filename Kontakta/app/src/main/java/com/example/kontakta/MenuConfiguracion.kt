470e19ce12c2260397f522f5588ec7157d7fd4b1
hradium <hazerful@live.com>
2021-06-14 18:38:53 -0500

    se agrega opcion de borrado de servicio del usuario loggeado

--- Kontakta/app/src/main/java/com/example/kontakta/MenuConfiguracion.kt
+++ Kontakta/app/src/main/java/com/example/kontakta/MenuConfiguracion.kt
@@ -5,7 +5,17 @@ import android.media.Image
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class MenuConfiguracion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +24,8 @@ class MenuConfiguracion : AppCompatActivity() {
         val buttonCam: Button = findViewById(R.id.buttonCUser) as Button
         var correo : String = intent.getStringExtra("correo").toString()
         var IDUser : String = intent.getStringExtra("IDUsuario").toString()
+        println("====================================IDUSER ENN CONFFF: $IDUser")
+
         val buttonInicio: ImageButton = findViewById(R.id.confInicioButt) as ImageButton
         buttonInicio.setOnClickListener{
             val intent = Intent(this,MenuPrincipal::class.java)
@@ -49,6 +61,11 @@ class MenuConfiguracion : AppCompatActivity() {
             intent.putExtra("IDUsuario", IDUser);
             startActivity(intent)
         }
+        val buttonBorrarServ: Button = findViewById(R.id.buttonBorrarServ) as Button
+        buttonBorrarServ.setOnClickListener{
+            nullSetUser(IDUser)
+
+        }
         var buttUPerfil: Button = findViewById(R.id.testVerU) as Button
         buttUPerfil.setOnClickListener{
             val intent1 = Intent(this, perfilUsuario::class.java)
@@ -61,5 +78,75 @@ class MenuConfiguracion : AppCompatActivity() {
             intent1.putExtra("correo", correo);
             startActivity(intent1)
         }
+    }
+
+    private fun nullSetUser(IDUser: String) {
+        val queue = Volley.newRequestQueue(this);
+        var listview = findViewById<ListView>(R.id.listViewHistorial)
+        var list = mutableListOf<Model>()
+        //val url = "http://192.168.1.45/kontakta/v1/getServ.php"
+        //val url = "http://192.168.1.109/kontakta/v1/getServ.php"
+        val url = "http://192.168.100.6/v1/nullSetUser.php"
+
+        //creating volley string request
+        val stringRequest = object : StringRequest(
+            Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                try {
+
+                    deleteServicio(IDUser)
+
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener { volleyError -> Toast.makeText(applicationContext, volleyError.message, Toast.LENGTH_LONG).show() }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                //Esta funcion es la que pone los parametros en el php, aqui le pasas lo que va a ocupar el php
+                params.put("IDUsuario", IDUser)
+                return params
+            }
+        }
+        //adding request to queue
+        queue.add(stringRequest);
+    }
+
+    private fun deleteServicio(IDUser: String) {
+        val queue = Volley.newRequestQueue(this);
+        var listview = findViewById<ListView>(R.id.listViewHistorial)
+        var list = mutableListOf<Model>()
+        //val url = "http://192.168.1.45/kontakta/v1/getServ.php"
+        //val url = "http://192.168.1.109/kontakta/v1/getServ.php"
+        val url = "http://192.168.100.6/v1/borrarServ.php"
+
+        //creating volley string request
+        val stringRequest = object : StringRequest(
+            Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                try {
+
+                    val intent1 = Intent(this, MenuPrincipal::class.java)
+                    intent1.putExtra("IDUsuario", IDUser);
+                    startActivity(intent1)
+
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener { volleyError -> Toast.makeText(applicationContext, volleyError.message, Toast.LENGTH_LONG).show() }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                //Esta funcion es la que pone los parametros en el php, aqui le pasas lo que va a ocupar el php
+                params.put("IDUsuario_FK", IDUser)
+                return params
+            }
+        }
+        //adding request to queue
+        queue.add(stringRequest);
     }
 }
