9b9253cee22c797fa2d7fc740ad83b7b61748585
hradium <hazerful@live.com>
2021-06-13 22:03:12 -0500

    agrego getHistorialUser.php

---
+++ kontaktaphp/v1/getHistorialUser.php
@@ -0,0 +1,27 @@
+<?php 
+	
+	require_once '../DbConnect.php';
+   $db = new DbConnect();   
+   $con = $db->connect();
+   
+   $array = array();	
+   if(isset($_POST['IDUsuario_FK'])){
+      	$IDUsuario_FK = $_POST['IDUsuario_FK'];   		
+     	$sql = "SELECT * FROM historialcontacto WHERE	IDUsuario_FK= '$IDUsuario_FK'";
+   if($resultset = mysqli_query($con, $sql)){
+	while ($row = mysqli_fetch_assoc($resultset)){
+		$e = array();
+		$e['IDHistCont'] = $row['IDHistCont'];
+		$e['nombrePrestador'] = $row['nombrePrestador'];
+		$e['imagenPrestador'] = $row['imagenPrestador'];	
+      array_push($array,$e);
+	}
+   }
+   }
+   else{
+      $array['message'] = "No se puede conectar";
+   }
+   echo json_encode($array);
+
+?>
+
