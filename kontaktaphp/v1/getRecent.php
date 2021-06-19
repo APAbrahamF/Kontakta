3e09ccdaea6f135dab4573bd7fac9cd2930505e7
hradium <hazerful@live.com>
2021-06-19 15:55:58 -0500

    agrego getRecent.php

---
+++ kontaktaphp/v1/getRecent.php
@@ -0,0 +1,20 @@
+<?php
+include('funcionesGET.php');
+//$tipo = $_GET['txtTi'];
+$array = array();	
+if($resultset=getSQLResultSet("SELECT * FROM prestadorservicios ORDER BY IDServicio DESC")){
+	
+	while ($row = $resultset->fetch_array(MYSQLI_NUM)){
+		$e = array();
+                $e['IDServicio'] = $row[0];
+                $e['nombreServicio'] = $row[1];
+                $e['imagen'] = $row[2];
+                $e['integrantes'] = $row[3];
+                $e['descripcion'] = $row[4];
+				$e['genero'] = $row[5];
+				$e['promedio'] = $row[10];
+                array_push($array,$e);
+	}
+        echo json_encode($array);
+}
+?>
